6cc1f17b9bebfbdcdad2938a9c3c9c96d82b0bdf
Ajand Mardalizad <aseedgeek@gmail.com>
2022-07-20 18:39:16 +0430

    creator form template added

---
+++ client/components/CreatorForm.js
@@ -0,0 +1,43 @@
+import {
+  Box,
+  Avatar,
+  Input,
+  Textarea,
+  VStack,
+  VisuallyHidden,
+} from "@chakra-ui/react";
+
+const CreatorForm = () => {
+  return (
+    <VStack py="10" bg="gray.200" width="90%" maxWidth="600px" spacing={4}>
+      <Box>
+        <VisuallyHidden>
+          <input name="avatar" id="avatar" display="hidden" type="file" />
+        </VisuallyHidden>
+        <label for="avatar">
+          <Avatar cursor="pointer" width="90px" height="90px" />
+        </label>
+      </Box>
+      <Box width="90%">
+        <Input
+          bg="white"
+          placeholder="Display Name"
+          width="100%"
+          focusBorderColor="blue.600"
+          color="gray.700"
+        />
+      </Box>
+      <Box width="90%">
+        <Textarea
+          focusBorderColor="blue.600"
+          color="gray.700"
+          bg="white"
+          placeholder="Bio"
+          width="100%"
+        />
+      </Box>
+    </VStack>
+  );
+};
+
+export default CreatorForm;
---
+++ client/pages/setup.js
@@ -0,0 +1,12 @@
+import { Center } from "@chakra-ui/react";
+import CreatorForm from "../components/CreatorForm";
+
+const Setup = () => {
+  return (
+    <Center h="100vh" bg='gray.100'>
+      <CreatorForm />
+    </Center>
+  );
+};
+
+export default Setup;
