a3d4a1a343d73ff7ac1b1c5dfadbc0e7bea8cb0e
Ajand Mardalizad <aseedgeek@gmail.com>
2022-07-23 18:42:02 +0430

    lit context and provider added

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,11 +1,15 @@
 // pages/_app.js
+import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { LitProvider } from "../lib/LitProvider";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <LitProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </LitProvider>
   );
 }
 
