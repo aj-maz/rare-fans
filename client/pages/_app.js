7a8c7176776814c397e6717a700d697c6b6a4c28
Ajand Mardalizad <aseedgeek@gmail.com>
2022-07-24 19:42:42 +0430

    metamask connection added

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,15 +2,25 @@
 import { useEffect } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { LitProvider } from "../lib/LitProvider";
+import { Web3ReactProvider } from "@web3-react/core";
+import { ethers, Web3 } from "ethers";
+
 import "../styles/globals.css";
+
+function getLibrary(provider) {
+  const target = new ethers.providers.Web3Provider(provider);
+  return target;
+}
 
 function MyApp({ Component, pageProps }) {
   return (
-    <LitProvider>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </LitProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <LitProvider>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </LitProvider>
+    </Web3ReactProvider>
   );
 }
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,75 @@
 import { Button, Container, VStack, Center, Box } from "@chakra-ui/react";
 import CreatorItem from "../components/CreatorItem";
+import { injected } from "../lib/connectors";
+import { useWeb3React } from "@web3-react/core";
+import { useEffect } from "react";
 
 const Home = () => {
+  const { active, account, library, connector, activate, deactivate } =
+    useWeb3React();
+
+  console.log(active, account, library, connector);
+
+  useEffect(() => {
+    const changeChain = (chainId) => {
+      console.log(chainId);
+      if (chainId !== "0x539") {
+        window.ethereum.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainId: "0x13881",
+              chainName: "Mumbai Testnet",
+              nativeCurrency: {
+                name: "MATIC",
+                symbol: "MATIC",
+                decimals: 18,
+              },
+              rpcUrls: ["https://rpc-mumbai.maticvigil.com"],
+              blockExplorerUrls: ["https://mumbai.polygonscan.com"],
+            },
+          ],
+        });
+      }
+    };
+
+    const main = async () => {
+      const chainId = await ethereum.request({ method: "eth_chainId" });
+
+      if (connector) {
+        changeChain(chainId);
+      }
+    };
+
+    main();
+
+    window.ethereum.on("connect", ({ chainId }) => {
+      changeChain(chainId);
+    });
+  }, [connector]);
+
+  const connect = async (walletType) => {
+    try {
+      await activate(walletType);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Box bg="gray.100" h="100vh" p="10">
       <Container bg="gray.100">
         <Center mt="20" mb="8">
-          <Button colorScheme="blue"> Signup as a Creator</Button>
+          <Button
+            onClick={async () => {
+              await connect(injected);
+              //await deactivate()
+            }}
+            colorScheme="blue"
+          >
+            {" "}
+            Signup as a Creator
+          </Button>
         </Center>
         <VStack spacing="20px" maxH="60vh" overflow="auto">
           <CreatorItem />
